ec4528afe7ddca2b9b40526b952a6969c025e983
Reza <rezakam64@yahoo.com>
2015-11-24 00:13:34 +0330

    New Retrofit

--- app/src/main/java/com/example/reza/firstapp/MainActivity.java
+++ app/src/main/java/com/example/reza/firstapp/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     int n=1;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/reza/firstapp/RVAdapter.java
+++ app/src/main/java/com/example/reza/firstapp/RVAdapter.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PersonViewHolder> {
 
 
+    
     public static class PersonViewHolder extends RecyclerView.ViewHolder {
 
         CardView cv;
